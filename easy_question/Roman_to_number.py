446bab559f3453134500459e1ca29281fa7b4dc9
Unheat <100108666+Unheat@users.noreply.github.com>
2024-10-03 12:26:05 -0400

    Update Roman_to_number.py

--- easy_question/Roman_to_number.py
+++ easy_question/Roman_to_number.py
@@ -1,1 +1,18 @@
-
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        convertTable = {
+            "I":1,
+            "V":5,
+            "X":10,
+            "L":50,
+            "C":100,
+            "D":500,
+            "M":1000,
+        }
+        sum = 0
+        for first_letter, letter in zip(s, s[1:]):
+            if convertTable[first_letter]<convertTable[letter]:
+                sum -= convertTable[first_letter]
+            else: sum += convertTable[first_letter]
+        sum += convertTable[s[-1]]
+        return sum
